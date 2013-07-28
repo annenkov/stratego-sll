67b2cbf9f4f09d57f6dd898e1a739b7f7141afb6
Danil Annenkov <annenkov@ib-soft.ru>
2013-07-28 23:41:27 +0900

    Added tracing. Cleaned code.

--- syntax/SLL.pp
+++ syntax/SLL.pp
@@ -1,5 +1,7 @@
 %% Pretty printing table (see also SLL.generated.pp)
 [
+   FCall                 -- H hs=0 [_1 KW["("] _2 KW[")"]],
+   FCall.2:iter-star-sep -- _1 KW[","],
    Ctor                 -- H hs=0 [_1 KW ["("] _2 KW[")"]],
    Ctor.2:iter-star-sep -- _1 KW [","]
 ]
